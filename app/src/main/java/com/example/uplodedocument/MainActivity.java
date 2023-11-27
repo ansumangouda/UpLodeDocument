05ef30e4b6706c6111a4e9704c8caa81cb2785d5
Ansuman Gouda <ansumangouda72@gmail.com>
2023-11-27 10:29:45 +0530

    PDF

--- app/src/main/java/com/example/uplodedocument/MainActivity.java
+++ app/src/main/java/com/example/uplodedocument/MainActivity.java
@@ -1,25 +1,49 @@
 package com.example.uplodedocument;
+import static android.app.PendingIntent.getActivity;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Objects;
+
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -98,17 +122,75 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getResultCode() == Activity.RESULT_OK){
                     Intent data = result.getData();
                     uri = data.getData();
+                    choosetextFild.setText(uri.toString());
                     imageView.setImageURI(uri);
-
                 }
             }
         });
-
-
-
     }
 
-    private void upLodeToServer(Uri uri) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+
+        if (requestCode == 2000 && resultCode == Activity.RESULT_OK) {
+            Uri uri  =  resultData.getData();
+
+            getContentResolver().takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//            String stringUri = uri.toString();
+//            choosetextFild.setText(stringUri);
+//            getPDF(uri);
+            try {
+                File file = getInputStreamFromUri(uri);
+                System.out.println("File uri name : "+file.getName());
+                System.out.println("File uri path is : "+file.getAbsolutePath());
+                uploadFile(file);
+            }  catch (Exception  e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public File getInputStreamFromUri(Uri uri) throws IOException {
+
+        InputStream inputStream= this.getContentResolver().openInputStream(uri);
+        File file = new File(this.getFilesDir(), "upload.pdf");
+
+        if (inputStream != null) {
+            try (OutputStream outputStream = Files.newOutputStream(file.toPath())) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+            } catch (Exception  e) {
+                System.out.println("File throws exception with" + e.getMessage());
+            } finally {
+                inputStream.close();
+            }
+        }
+        return file;
+    }
+
+
+
+    // Request code for selecting a PDF document.
+    private static final int PICK_PDF_FILE = 2;
+
+    private void openFile(Uri pickerInitialUri) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, PICK_PDF_FILE);
+    }
+
+    private void upLodeToServer(Uri  uri) {
         if (uri != null) {
             String filePath = getRealPathFromUri(uri);
             if (filePath != null) {
@@ -141,6 +223,37 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "URI is null", Toast.LENGTH_SHORT).show();
         }
 
+
+    }
+    private void uploadFile(File file) {
+            if (file != null) {
+                System.out.println("File upload started with  file name: "+file.getName()+ "File path" +file.getAbsolutePath());
+                RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), file);
+                RequestBody Doc_Type_Id_FK = RequestBody.create(MediaType.parse("text/plain"), String.valueOf(3));
+                RequestBody PR_Id_FK = RequestBody.create(MediaType.parse("text/plain"), String.valueOf(2336));
+                MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
+
+                Call<DummyClass> call = apiService.insertDocement(filePart, Doc_Type_Id_FK, PR_Id_FK);
+                call.enqueue(new Callback<DummyClass>() {
+                    @Override
+                    public void onResponse(Call<DummyClass> call, Response<DummyClass> response) {
+                        System.out.println("File upload ended successfully with  file name: "+file.getName()+ "File path" +file.getAbsolutePath());
+
+                        if (response.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "susses", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<DummyClass> call, Throwable t) {
+                        System.out.println("File upload ended failed with  file name: "+file.getName()+ "File path" +file.getAbsolutePath());
+
+                    }
+                });
+            }else {
+                // Handle the case where the filePath is null
+                Toast.makeText(this, "File path is null", Toast.LENGTH_SHORT).show();
+            }
 
     }
 
@@ -195,6 +308,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return filePath;
     }
+
     private String getRealPathFromUriKitKatAndAbove(Uri uri){
         String filepath = null;
         try {
@@ -218,14 +332,93 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             if (v.getId() == R.id.chooseFile){
-                Intent iGallery = new Intent(Intent.ACTION_GET_CONTENT);
-                iGallery.setType("image/jpg");
-                iGallery.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
-                activityResultLauncher.launch(iGallery);
+                pdfOpen();
             }
-            if (v.getId() == R.id.saveBtn){
-                upLodeToServer(uri);
+            if (v.getId() == R.id.saveBtn) {
+//                upLodeToServer(uri);
             }
         }
+    }
+
+    private void openMedia() {
+        Intent iGallery = new Intent(Intent.ACTION_GET_CONTENT);
+        iGallery.setType("image/jpg");
+        iGallery.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+        activityResultLauncher.launch(iGallery);
+    }
+
+    private void pdfOpen() {
+        Intent iGallery = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        iGallery.addCategory(Intent.CATEGORY_OPENABLE);
+        iGallery.setType("application/pdf");
+        iGallery.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+        activityResultLauncher.launch(iGallery);
+        startActivityForResult(iGallery, 2000);
+    }
+
+    private void getPDFFileSize(Uri uri)  {
+
+            // The query, because it only applies to a single document, returns only
+            // one row. There's no need to filter, sort, or select fields,
+            // because we want all fields for one document.
+            Cursor cursor = this.getContentResolver()
+                    .query(uri, null, null, null, null, null);
+
+            try {
+                // moveToFirst() returns false if the cursor has 0 rows. Very handy for
+                // "if there's anything to look at, look at it" conditionals.
+                if (cursor != null && cursor.moveToFirst()) {
+
+                    // Note it's called "Display Name". This is
+                    // provider-specific, and might not necessarily be the file name.
+                    @SuppressLint("Range") String displayName = cursor.getString(
+                            cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                    Log.i(TAG, "Display Name: " + displayName);
+
+                    int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+                    // If the size is unknown, the value stored is null. But because an
+                    // int can't be null, the behavior is implementation-specific,
+                    // and unpredictable. So as
+                    // a rule, check if it's null before assigning to an int. This will
+                    // happen often: The storage API allows for remote files, whose
+                    // size might not be locally known.
+                    String size = null;
+                    if (!cursor.isNull(sizeIndex)) {
+                        // Technically the column stores an int, but cursor.getString()
+                        // will do the conversion automatically.
+                        size = cursor.getString(sizeIndex);
+                    } else {
+                        size = "Unknown";
+                    }
+                    Log.i(TAG, "Size: " + size);
+                }
+            }catch (Exception  e){
+
+            } finally {
+                cursor.close();
+            }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                     getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
